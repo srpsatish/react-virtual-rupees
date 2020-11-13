5356845e39185013bba31c69d81406099ee10aa6
Satish Singh <singhsatish7275@gmail.com>
2020-11-13 11:11:42 +0530

    navbar updated and sidebar added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <>
+          <Nav>
+              <NavbarContainer>
+                  <NavLogo to="/">₹upees</NavLogo>
+                  <MobileIcon>
+                      <FaBars />
+                  </MobileIcon>
+                  <NavMenu>
+                      <NavItem>
+                          <NavLinks to="about">About</NavLinks>
+                      </NavItem>
+                      <NavItem>
+                          <NavLinks to="discover">Discover</NavLinks>
+                      </NavItem>
+                      <NavItem>
+                          <NavLinks to="services">Services</NavLinks>
+                      </NavItem>
+                      <NavItem>
+                          <NavLinks to="signup">Sign Up</NavLinks>
+                      </NavItem>
+                  </NavMenu>
+                  <NavBtn>
+                      <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                  </NavBtn>
+              </NavbarContainer>
+          </Nav>
+        </>
     )
 }
 
